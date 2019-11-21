b45dba1819b5cdd7994c119fccb9ae8b50765443
adithyasripada <55552063+adithyasripada@users.noreply.github.com>
2019-11-21 21:28:11 +0000

    drew player and coin objects

---
+++ JS/GameObjects.js
@@ -0,0 +1,12 @@
+
+Obj.beginPath();
+Obj.rect(450,550,45,45);
+Obj.fillStyle = "#D3A3FF";
+Obj.fill();
+Obj.closePath();
+
+Obj.beginPath();
+Obj.arc(345, 30, 5, 0, Math.PI*2, false);
+Obj.fillStyle = "#D3A3FF";
+Obj.fill();
+Obj.closePath();
