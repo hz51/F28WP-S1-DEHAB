c7de3c5beaedb5e63f6729863bf92b604190cb13
hz51 <55840528+hz51@users.noreply.github.com>
2019-11-24 16:25:26 +0000

    Works now...
    However, there is more i need to do this is a previos itteration of what i was doing becasue it suddenly broke and i dont know when i broke it, will have it done when i get home from work

---
+++ JS/Map.js
@@ -0,0 +1,403 @@
+var map = {
+    cols: 24,
+    rows: 24,
+    tsize: 64,
+    layers: [[
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+        ], [
+        4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4,
+        4, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4,
+        4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+        4, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4,
+        ]],
+    tileCreate: function (layer, col, row) {
+        //returns the first list as layer, and the second list as the number of rows * by colums then adding the actual colum values in to create the moveable map
+        return this.layers[layer][row * 24 + col];
+    },
+    isSolidTileAtXY: function (x, y) {
+        //makes sure tiles are solid by dividing the x quard by the size of the tile(64)
+        var col = Math.floor(x / 64);
+        var row = Math.floor(y / 64);
+
+        // 2, is a solid tile and cannot be moved onto
+        // loop through all layers and return TRUE if any tile is solid
+        return this.layers.reduce(function (res, layer, index) {
+            var tile = this.tileCreate(index, col, row);
+            var canMove = (tile === 2) || (tile === 3) || (tile === 4) || (tile == 5)
+            return res || canMove;
+        //Binds this to false as to make sure that the char can move after it collides but not in that same direction
+        }.bind(this), false);
+    },
+    getCol: function (x) {
+        return Math.floor(x / 64);
+    },
+    getRow: function (y) {
+        return Math.floor(y / 64);
+    },
+    getX: function (col) {
+        return col * 64;
+    },
+    getY: function (row) {
+        return row * 64;
+    }
+};
+//Creates a Camera
+function Camera(map, width, height) {
+    this.x = 0;
+    this.y = 0;
+    //if i use 500(the size of the canvas we are using it )
+    this.width = width;
+    this.height = height;
+    //sets limit of where the camera can go
+    this.maxX = map.cols * 64 - width;
+    this.maxY = map.rows * 64 - height;
+}
+//makes the char move with the camera
+Camera.prototype.follow = function (Charact) {
+    this.following = Charact;
+    Charact.screenX = 0;
+    Charact.screenY = 0;
+};
+
+Camera.prototype.update = function () {
+    // character movement
+    this.following.screenX = this.width / 2;
+    this.following.screenY = this.height / 2;
+
+    // make the camera follow the Charact by using function above
+    this.x = this.following.x - this.width / 2;
+    this.y = this.following.y - this.height / 2;
+    // clamp values
+    this.x = Math.max(0, Math.min(this.x, this.maxX));
+    this.y = Math.max(0, Math.min(this.y, this.maxY));
+
+    // left and right sides
+    if (this.following.x < this.width / 2 ||
+        this.following.x > this.maxX + this.width / 2) {
+        this.following.screenX = this.following.x - this.x;
+    }
+    // top and bottom sides
+    if (this.following.y < this.height / 2 ||
+        this.following.y > this.maxY + this.height / 2) {
+        this.following.screenY = this.following.y - this.y;
+    }
+};
+//creats an object
+var Loader = {
+    images: {}
+};
+//Keys used to create more then one instance of the variable that is easy to use
+Loader.loadImage = function (key, src) {
+    var img = new Image();
+
+    var temp = new Promise(function (resolve, reject) {
+        img.onload = function () {
+            this.images[key] = img;
+            resolve(img);
+        //bind used to set set property of images
+        }.bind(this);
+    //bind used same as before
+    }.bind(this));
+
+    img.src = src;
+    return temp;
+};
+//Loads image
+Loader.getImage = function (key) {
+    //returns image looking for or returns nothing
+    if(key in this.images)
+        return this.images[key]
+    else 
+        return null
+}
+
+
+function hero(map, x, y) {
+    this.map = map;
+    this.x = x;
+    this.y = y;
+    this.width = map.tsize;
+    this.height = map.tsize;
+
+    this.image = Loader.getImage('hero');
+}
+
+hero.SPEED = 256; // pixels per second
+
+hero.prototype.move = function (delta, dirx, diry) {
+    // move hero
+    this.x += dirx * hero.SPEED * delta;
+    this.y += diry * hero.SPEED * delta;
+
+    // check if we walked into a non-walkable tile
+    this._collide(dirx, diry);
+
+    // clamp values
+    var maxX = this.map.cols * this.map.tsize;
+    var maxY = this.map.rows * this.map.tsize;
+    this.x = Math.max(0, Math.min(this.x, maxX));
+    this.y = Math.max(0, Math.min(this.y, maxY));
+};
+
+hero.prototype._collide = function (dirx, diry) {
+    var row, col;
+    // -1 in right and bottom is because image ranges from 0..63
+    // and not up to 64
+    var left = this.x - this.width / 2;
+    var right = this.x + this.width / 2 - 1;
+    var top = this.y - this.height / 2;
+    var bottom = this.y + this.height / 2 - 1;
+
+    // check for collisions on sprite sides
+    var collision =
+        this.map.isSolidTileAtXY(left, top) ||
+        this.map.isSolidTileAtXY(right, top) ||
+        this.map.isSolidTileAtXY(right, bottom) ||
+        this.map.isSolidTileAtXY(left, bottom);
+    if (!collision) { return; }
+
+    if (diry > 0) {
+        row = this.map.getRow(bottom);
+        this.y = -this.height / 2 + this.map.getY(row);
+    }
+    else if (diry < 0) {
+        row = this.map.getRow(top);
+        this.y = this.height / 2 + this.map.getY(row + 1);
+    }
+    else if (dirx > 0) {
+        col = this.map.getCol(right);
+        this.x = -this.width / 2 + this.map.getX(col);
+    }
+    else if (dirx < 0) {
+        col = this.map.getCol(left);
+        this.x = this.width / 2 + this.map.getX(col + 1);
+    }
+};
+var Game = {};
+
+Game.run = function (context) {
+    this.ctx = context;
+    this._previousElapsed = 0;
+
+    var p = this.load();
+    Promise.all(p).then(function (loaded) {
+        this.init();
+        window.requestAnimationFrame(this.tick);
+    }.bind(this));
+};
+
+Game.tick = function (elapsed) {
+    window.requestAnimationFrame(this.tick);
+
+    // clear previous frame
+    this.ctx.clearRect(0, 0, 512, 512);
+
+    // compute delta time in seconds -- also cap it
+    var delta = (elapsed - this._previousElapsed) / 1000.0;
+    delta = Math.min(delta, 0.25); // maximum delta of 250 ms
+    this._previousElapsed = elapsed;
+
+    this.update(delta);
+    this.render();
+}.bind(Game);
+
+
+
+
+
+
+
+
+// start up function
+
+
+window.onload = function () {
+    var context = document.getElementById('game').getContext('2d');
+    Game.run(context);
+};
+
+//
+// Keyboard handler
+//
+
+var Keyboard = {};
+
+Keyboard.LEFT = 37;
+Keyboard.RIGHT = 39;
+Keyboard.UP = 38;
+Keyboard.DOWN = 40;
+
+Keyboard._keys = {};
+
+Keyboard.listenForEvents = function (keys) {
+    window.addEventListener('keydown', this._onKeyDown.bind(this));
+    window.addEventListener('keyup', this._onKeyUp.bind(this));
+
+    keys.forEach(function (key) {
+        this._keys[key] = false;
+    }.bind(this));
+}
+
+Keyboard._onKeyDown = function (event) {
+    var keyCode = event.keyCode;
+    if (keyCode in this._keys) {
+        event.preventDefault();
+        this._keys[keyCode] = true;
+    }
+};
+
+Keyboard._onKeyUp = function (event) {
+    var keyCode = event.keyCode;
+    if (keyCode in this._keys) {
+        event.preventDefault();
+        this._keys[keyCode] = false;
+    }
+};
+
+Keyboard.isDown = function (keyCode) {
+    if (!keyCode in this._keys) {
+        throw new Error('Keycode ' + keyCode + ' is not being listened to');
+    }
+    return this._keys[keyCode];
+};
+
+Game.load = function () {
+    return [
+        Loader.loadImage('Map', '../assets/Map.png'),
+        Loader.loadImage('hero', '../assets/character.png')
+    ];
+};
+
+Game.init = function () {
+    Keyboard.listenForEvents(
+        [Keyboard.LEFT, Keyboard.RIGHT, Keyboard.UP, Keyboard.DOWN]);
+    this.tileAtlas = Loader.getImage('Map');
+
+    this.hero = new hero(map, 160, 160);
+    this.camera = new Camera(map, 512, 512);
+    this.camera.follow(this.hero);
+};
+
+Game.update = function (delta) {
+    // handle hero movement with arrow keys
+    var dirx = 0;
+    var diry = 0;
+    if (Keyboard.isDown(Keyboard.LEFT)) { dirx = -1; }
+    else if (Keyboard.isDown(Keyboard.RIGHT)) { dirx = 1; }
+    else if (Keyboard.isDown(Keyboard.UP)) { diry = -1; }
+    else if (Keyboard.isDown(Keyboard.DOWN)) { diry = 1; }
+
+    this.hero.move(delta, dirx, diry);
+    this.camera.update();
+};
+
+Game._drawLayer = function (layer) {
+    var startCol = Math.floor(this.camera.x / map.tsize);
+    var endCol = startCol + (this.camera.width / map.tsize);
+    var startRow = Math.floor(this.camera.y / map.tsize);
+    var endRow = startRow + (this.camera.height / map.tsize);
+    var offsetX = -this.camera.x + startCol * map.tsize;
+    var offsetY = -this.camera.y + startRow * map.tsize;
+
+    for (var c = startCol; c <= endCol; c++) {
+        for (var r = startRow; r <= endRow; r++) {
+            var tile = map.tileCreate(layer, c, r);
+            var x = (c - startCol) * map.tsize + offsetX;
+            var y = (r - startRow) * map.tsize + offsetY;
+            if (tile !== 0) { // 0 => empty tile
+                this.ctx.drawImage(
+                    this.tileAtlas, // image
+                    (tile - 1) * map.tsize, // source x
+                    0, // source y
+                    map.tsize, // source width
+                    map.tsize, // source height
+                    Math.round(x),  // target x
+                    Math.round(y), // target y
+                    map.tsize, // target width
+                    map.tsize // target height
+                );
+            }
+        }
+    }
+};
+
+Game._drawGrid = function () {
+        var width = map.cols * map.tsize;
+    var height = map.rows * map.tsize;
+    var x, y;
+    for (var r = 0; r < map.rows; r++) {
+        x = - this.camera.x;
+        y = r * map.tsize - this.camera.y;
+        this.ctx.beginPath();
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(width, y);
+        this.ctx.stroke();
+    }
+    for (var c = 0; c < map.cols; c++) {
+        x = c * map.tsize - this.camera.x;
+        y = - this.camera.y;
+        this.ctx.beginPath();
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(x, height);
+        this.ctx.stroke();
+    }
+};
+
+Game.render = function () {
+    // draw map background layer
+    this._drawLayer(0);
+
+    // draw main character
+    this.ctx.drawImage(
+        this.hero.image,
+        this.hero.screenX - this.hero.width / 2,
+        this.hero.screenY - this.hero.height / 2);
+
+    // draw map top layer
+    this._drawLayer(1);
+
+    this._drawGrid();
+};
